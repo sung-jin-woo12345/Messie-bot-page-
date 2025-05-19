d399b4c5604619e82dee09fdefd8ac29e5ea6c75
sung-jin-woo12345 <198474287+sung-jin-woo12345@users.noreply.github.com>
2025-05-19 23:08:34 +0100

    Create prefix.js

---
+++ commands/prefix.js
@@ -0,0 +1,26 @@
+const { PREFIX } = require('../handles/handleMessage');
+const { sendMessage } = require('../handles/sendMessage');
+
+module.exports = {
+  name: 'prefix',
+  description: 'Affiche le préfixe actuel du bot',
+  usage: 'prefix',
+  author: 'Messie Osango',
+  execute(senderId, args, pageAccessToken) {
+    const prefixMessage = `
+╭─⌾⋅ ミ✘.𝙼𝙴𝚂𝚂𝙸𝙴 ⋅⌾──╮
+│
+│   𝗣𝗿𝗲́𝗳𝗶𝘅𝗲 𝗮𝗰𝘁𝘂𝗲𝗹 𝗱𝘂 𝗯𝗼𝘁 :
+│
+│   ✧ 「 ${PREFIX} 」
+│
+│   𝚄𝚝𝚒𝚕𝚒𝚜𝚎𝚣 ${PREFIX}help 𝚙𝚘𝚞𝚛
+│   𝚟𝚘𝚒𝚛 𝚝𝚘𝚞𝚜 𝚕𝚎𝚜 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚎𝚜
+│
+│   
+│
+╰─────⌾⋅  ⋅⌾─────╯`;
+
+    sendMessage(senderId, { text: prefixMessage }, pageAccessToken);
+  }
+};
